a8ae6ab744b5f54d87e016d8b4d98ee9ecb4af9a
Moniika <48659321+moniika21@users.noreply.github.com>
2022-10-28 15:47:33 +0200

    Update installation-softwares.sh

--- installation-softwares.sh
+++ installation-softwares.sh
@@ -7,3 +7,4 @@ apt install -t stable-backports firmware-misc-nonfree
 apt install -t stable-backports nvidia-cuda-dev
 apt install -t stable-backports nvidia-cuda-toolkit
 apt install htop iftop
+# curl -fsSL 'http://ftp.fr.debian.org/debian/dists/stable-backports/Release' | grep -E '^Suite|^Codename'
